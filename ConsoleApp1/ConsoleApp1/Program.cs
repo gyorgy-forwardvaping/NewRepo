7264d92797889de187dda3a5f77e0c6bdf30b91f
Gyorgy Jezerniczky <jezerniczky.gy@gmail.com>
2023-08-05 08:20:59 +0100

    drawing X for fun

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int n = 8;
+for (int i = 0; i < n; i++)
+{   
+    String stars = "";
+    for (int j = 0; j < n; j++) 
+    {
+        if (j == i || j == 0 || j == n-1 || j == n - i -1 || i == 0 || i == n-1) {
+            stars += "*";
+        } else { 
+            stars += " "; 
+        }
+    }
+    
+    Console.WriteLine(stars);
+}
